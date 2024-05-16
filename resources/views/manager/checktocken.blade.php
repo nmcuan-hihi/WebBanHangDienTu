d8e0f4b292e8fd4e902c88fa3812a215d93a051f
nmcuan-hihi <nguyennnminhhhquannn@gmail.com>
2024-05-17 04:59:39 +0700

    demo full all web

--- resources/views/manager/checktocken.blade.php
+++ resources/views/manager/checktocken.blade.php
@@ -1,13 +1,55 @@
-@extends('nav.header')
+<!DOCTYPE html>
+<html>
 
-@section('content')
+<head>
+    <title>NMCUAN-Demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        .fixed-top {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            z-index: 1000;
+        }
+    </style>
+</head>
+
+<body style="padding-top: 60px;">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+        <div class="container container-fluid">
+            <a class="navbar-brand" href="#">NMCUAN-HIHI</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav me-auto">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">Sign up</a>
+                    </li>
+
+            </div>
+        </div>
+    </nav>
+</body>
+
+</html>
+
 
 <div class="container mt-2">
-@if (Session::has('errors'))
+    @if (Session::has('errors'))
     <div class="alert alert-success">
         <i class="fas fa-check-circle"></i> {{ Session::get('errors') }}
     </div>
     @endif
+
     <body>
         <h1>Verify Token</h1>
         <form action="{{ route('manager') }}" method="POST">
@@ -19,4 +61,3 @@
         </form>
     </body>
 </div>
-@endsection
